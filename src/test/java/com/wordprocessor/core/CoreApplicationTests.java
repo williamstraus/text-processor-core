490c334c20e05d028d8f46bf75ce5feef03fc591
William Straus <wllam08@gmail.com>
2022-09-10 12:30:57 +0300

    Spring Boot initial setup

---
+++ src/test/java/com/wordprocessor/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wordprocessor.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
